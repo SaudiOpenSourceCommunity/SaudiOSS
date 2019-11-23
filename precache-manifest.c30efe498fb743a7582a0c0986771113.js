7bf508b0718a5700a248f2cad3ca7012df803119
Abdulaziz <alobodi1@hotmail.com>
2019-11-22 23:36:59 -0500

    Updates

---
+++ precache-manifest.c30efe498fb743a7582a0c0986771113.js
@@ -0,0 +1,226 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56649a9e5b34f8ee7319936d62120016",
+    "url": "/SaudiOSS/index.html"
+  },
+  {
+    "revision": "fa5ca400cc64aa8bf381",
+    "url": "/SaudiOSS/static/css/2.6f294a06.chunk.css"
+  },
+  {
+    "revision": "fcee9bc8c72cc4af1fdb",
+    "url": "/SaudiOSS/static/css/main.3ed300ab.chunk.css"
+  },
+  {
+    "revision": "fa5ca400cc64aa8bf381",
+    "url": "/SaudiOSS/static/js/2.df611ddd.chunk.js"
+  },
+  {
+    "revision": "fcee9bc8c72cc4af1fdb",
+    "url": "/SaudiOSS/static/js/main.bbb90652.chunk.js"
+  },
+  {
+    "revision": "e30c7cd60d998afc91ba",
+    "url": "/SaudiOSS/static/js/runtime-main.7ea02f8b.js"
+  },
+  {
+    "revision": "21c4d25c75a50d8871360343a8cab85d",
+    "url": "/SaudiOSS/static/media/Lato-Black.21c4d25c.woff"
+  },
+  {
+    "revision": "77d353744697c77955f9bacc7f3ed90a",
+    "url": "/SaudiOSS/static/media/Lato-Black.77d35374.ttf"
+  },
+  {
+    "revision": "9ab28490b471a4ecb5b009a8cf1e8ee1",
+    "url": "/SaudiOSS/static/media/Lato-Black.9ab28490.woff2"
+  },
+  {
+    "revision": "cb6fe25ec47ffbaef682b71b1b133b8a",
+    "url": "/SaudiOSS/static/media/Lato-Black.cb6fe25e.svg"
+  },
+  {
+    "revision": "f21ec1ef673d97424fa24c6569c61e2f",
+    "url": "/SaudiOSS/static/media/Lato-Black.f21ec1ef.eot"
+  },
+  {
+    "revision": "16d065a79427b54476fdb5b93e6412a2",
+    "url": "/SaudiOSS/static/media/Lato-BlackItalic.16d065a7.eot"
+  },
+  {
+    "revision": "3a9f8a8c8cc8facb4e0a83a0dea7329b",
+    "url": "/SaudiOSS/static/media/Lato-BlackItalic.3a9f8a8c.woff2"
+  },
+  {
+    "revision": "5fc797bf364b753dbb597806348a2fac",
+    "url": "/SaudiOSS/static/media/Lato-BlackItalic.5fc797bf.svg"
+  },
+  {
+    "revision": "6c522f09efc8b83271666585f9fc6bf0",
+    "url": "/SaudiOSS/static/media/Lato-BlackItalic.6c522f09.ttf"
+  },
+  {
+    "revision": "eb70fa8a58ab4cf49c42f84f23afcc8f",
+    "url": "/SaudiOSS/static/media/Lato-BlackItalic.eb70fa8a.woff"
+  },
+  {
+    "revision": "44dfe8cc676882243911a3197a50169e",
+    "url": "/SaudiOSS/static/media/Lato-Bold.44dfe8cc.ttf"
+  },
+  {
+    "revision": "5dfc529dfa53af18cf8db0ac408b3c19",
+    "url": "/SaudiOSS/static/media/Lato-Bold.5dfc529d.woff"
+  },
+  {
+    "revision": "a47d8f9cf715b23d1ed1da4054b56f5f",
+    "url": "/SaudiOSS/static/media/Lato-Bold.a47d8f9c.eot"
+  },
+  {
+    "revision": "cfc7a0efcf638de6ab09d0524a6c6758",
+    "url": "/SaudiOSS/static/media/Lato-Bold.cfc7a0ef.woff2"
+  },
+  {
+    "revision": "e05bae7c149974995f444c70be1c0aac",
+    "url": "/SaudiOSS/static/media/Lato-Bold.e05bae7c.svg"
+  },
+  {
+    "revision": "104d6a885096c90bfb643259a643d829",
+    "url": "/SaudiOSS/static/media/Lato-BoldItalic.104d6a88.woff2"
+  },
+  {
+    "revision": "15518f502f7f1a993f83e94c09acc976",
+    "url": "/SaudiOSS/static/media/Lato-BoldItalic.15518f50.eot"
+  },
+  {
+    "revision": "1ba4767ee37aab7e8d34fc339c3538cc",
+    "url": "/SaudiOSS/static/media/Lato-BoldItalic.1ba4767e.ttf"
+  },
+  {
+    "revision": "4a4cd2e7626ff79b2719e3221d7f2f46",
+    "url": "/SaudiOSS/static/media/Lato-BoldItalic.4a4cd2e7.svg"
+  },
+  {
+    "revision": "5a5f072318a76b880d5076eeaf1e7417",
+    "url": "/SaudiOSS/static/media/Lato-BoldItalic.5a5f0723.woff"
+  },
+  {
+    "revision": "9353e4f9cd72c87482497e661578957a",
+    "url": "/SaudiOSS/static/media/Lato-Hairline.9353e4f9.woff2"
+  },
+  {
+    "revision": "a0126eff0d671dd2898abe3593ea879f",
+    "url": "/SaudiOSS/static/media/Lato-Hairline.a0126eff.woff"
+  },
+  {
+    "revision": "cc02ace78557f1308c9382f9ce81b80d",
+    "url": "/SaudiOSS/static/media/Lato-Hairline.cc02ace7.eot"
+  },
+  {
+    "revision": "db15ac7981b2d2897d1a3c22892b5a51",
+    "url": "/SaudiOSS/static/media/Lato-Hairline.db15ac79.ttf"
+  },
+  {
+    "revision": "e5d453c9f92af2d3ad810b9507a213c0",
+    "url": "/SaudiOSS/static/media/Lato-Hairline.e5d453c9.svg"
+  },
+  {
+    "revision": "51e3f9585f4f66bee1ad9c5b488b46ca",
+    "url": "/SaudiOSS/static/media/Lato-HairlineItalic.51e3f958.woff"
+  },
+  {
+    "revision": "8f30b43840d0cadafe1347ec570458a0",
+    "url": "/SaudiOSS/static/media/Lato-HairlineItalic.8f30b438.svg"
+  },
+  {
+    "revision": "a567f4a8e101cb600004e09526a1a170",
+    "url": "/SaudiOSS/static/media/Lato-HairlineItalic.a567f4a8.ttf"
+  },
+  {
+    "revision": "d1b4f2b60c46b18d634524c24a20aa7e",
+    "url": "/SaudiOSS/static/media/Lato-HairlineItalic.d1b4f2b6.woff2"
+  },
+  {
+    "revision": "f5bafa9ae1e00250dd15255820777e83",
+    "url": "/SaudiOSS/static/media/Lato-HairlineItalic.f5bafa9a.eot"
+  },
+  {
+    "revision": "1ffe506636db6daac79dafb1100efe8d",
+    "url": "/SaudiOSS/static/media/Lato-Italic.1ffe5066.svg"
+  },
+  {
+    "revision": "3ecfbc65f009551edc7e58dd35a8a2ec",
+    "url": "/SaudiOSS/static/media/Lato-Italic.3ecfbc65.woff"
+  },
+  {
+    "revision": "56c4cb26fd6a48b9c0ebcc07b376ee38",
+    "url": "/SaudiOSS/static/media/Lato-Italic.56c4cb26.ttf"
+  },
+  {
+    "revision": "af5a576da8a3d664ac832355065cb8bd",
+    "url": "/SaudiOSS/static/media/Lato-Italic.af5a576d.eot"
+  },
+  {
+    "revision": "b441b7902bd4c1e6092e35ed1d287e56",
+    "url": "/SaudiOSS/static/media/Lato-Italic.b441b790.woff2"
+  },
+  {
+    "revision": "5b761f2d1e4259ea6ac7ab3ebf7f3c49",
+    "url": "/SaudiOSS/static/media/Lato-Light.5b761f2d.ttf"
+  },
+  {
+    "revision": "96b78ec9be61a9b1e91d455627636d53",
+    "url": "/SaudiOSS/static/media/Lato-Light.96b78ec9.woff2"
+  },
+  {
+    "revision": "9d57e0c919026e4d61c7f7087e43a00f",
+    "url": "/SaudiOSS/static/media/Lato-Light.9d57e0c9.woff"
+  },
+  {
+    "revision": "b13b187d608ae75fa657f27698a77054",
+    "url": "/SaudiOSS/static/media/Lato-Light.b13b187d.svg"
+  },
+  {
+    "revision": "f014a7e840bcd8fcdbaaba729f2847d9",
+    "url": "/SaudiOSS/static/media/Lato-Light.f014a7e8.eot"
+  },
+  {
+    "revision": "3d747d8bf464744eecb91556d86de11b",
+    "url": "/SaudiOSS/static/media/Lato-LightItalic.3d747d8b.ttf"
+  },
+  {
+    "revision": "75da51e5d9b7014a8456bc62e7e544e9",
+    "url": "/SaudiOSS/static/media/Lato-LightItalic.75da51e5.svg"
+  },
+  {
+    "revision": "88376c870233634fa4b208b40902bd5c",
+    "url": "/SaudiOSS/static/media/Lato-LightItalic.88376c87.eot"
+  },
+  {
+    "revision": "a0d2a3a8df176d2319d2fb8e009c8280",
+    "url": "/SaudiOSS/static/media/Lato-LightItalic.a0d2a3a8.woff2"
+  },
+  {
+    "revision": "e4669f8c91a258d8de12601f86154cce",
+    "url": "/SaudiOSS/static/media/Lato-LightItalic.e4669f8c.woff"
+  },
+  {
+    "revision": "3679ad95a1ac1277cd25d379a55712b5",
+    "url": "/SaudiOSS/static/media/Lato-Regular.3679ad95.woff2"
+  },
+  {
+    "revision": "39a3905085ad34aa621eaccdd6b70440",
+    "url": "/SaudiOSS/static/media/Lato-Regular.39a39050.eot"
+  },
+  {
+    "revision": "7f690e503a254e0b8349aec0177e07aa",
+    "url": "/SaudiOSS/static/media/Lato-Regular.7f690e50.ttf"
+  },
+  {
+    "revision": "e4fa05a4f0c9ec0fa8dd8d44fcb259a4",
+    "url": "/SaudiOSS/static/media/Lato-Regular.e4fa05a4.woff"
+  },
+  {
+    "revision": "e9d329fb99c7e3244dc344c9e0e422de",
+    "url": "/SaudiOSS/static/media/Lato-Regular.e9d329fb.svg"
+  }
+]);
