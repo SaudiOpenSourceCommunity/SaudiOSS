bfd07f0691991935fe13249c1229671a74eedb8b
Mohannad Otaibi <mohannad.otaibi@gmail.com>
2023-08-05 23:30:58 +0300

    Couple of edits
    - Rename actions
    - Update Update Readme Action
    - rename OSS-update-readme to `update_readme.js`
    - moved the script to `utilities` folder to keep root folder tidy
    - Refactored the script to modularize a bit

---
+++ utilities/update_readme.js
@@ -0,0 +1,118 @@
+const fs = require("fs");
+const path = require("path");
+const octokit = require("@octokit/core");
+
+const client = new octokit.Octokit({ auth: process.env.GITHUB_TOKEN });
+
+// Lets get the developers list
+const devs = require("../devs.json");
+
+// Lets get the template
+const template = fs.readFileSync(path.resolve(__dirname, "template.md")).toString();
+
+// Lets prepare the readme content
+const readmeContent = `
+  <table dir="rtl">
+    <tr>
+        <th>المبرمج</th>
+        <th>المشروع</th>
+        <th>الوصف</th>
+    </tr>
+    ${
+      devs.map(dev => `
+      <tr>
+          <td rowspan="${dev.projects.length}"><a href="${dev.githubURL}"> ${dev.name} </a></td>
+          ${dev.projects.map(project => `
+          <td><a href=${project.URL}>${project.name}</a></td>
+          <td>${project.description.trim() == "" ? "&nbsp;" : project.description.trim()}</td>
+      </tr>
+      `).join("")}`
+      ).join("")}
+  </table>`;
+
+// Lets append the content to the template
+const newTemplate = template.replace("<!-- DEVELOPERS LIST -->", readmeContent);
+
+const updateReadme = async (newTemplate) => {
+  try {
+    // Get the current readme to save the sha and encoding
+    const res = await client.request("GET /repos/SaudiOpenSourceCommunity/SaudiOSS/contents/README.md");
+    const { sha, encoding } = res.data;
+    
+    // Update the readme
+    client.request("PUT /repos/SaudiOpenSourceCommunity/SaudiOSS/contents/README.md",{
+        message: "Update readme.md",
+        content: Buffer.from(newTemplate, "utf-8").toString(encoding),
+        sha
+      }
+    );
+  } 
+  catch (error) {
+    console.error('updating readme', error);
+  }
+}
+
+const getProjectsDescriptions = async (developers) => {
+  await Promise.all(
+    developers.map(async developer => {
+      developer.projects = await Promise.all(
+        developer.projects.map(async project => {
+          if (project.details) return project;
+
+          const request_url = project.URL.replace(/https?:\/\/github\.com\//,"https://api.github.com/repos/").trim();
+          
+          try {
+            const response = await client.request(request_url);
+            const data = await response.json();
+
+            project.details = {
+              id: data.id,
+              language: data.language,
+              license: data.license,
+              topics: data.topics
+            };
+
+            console.log(`✅ Project ${project.name} by ${developer.name} has been extracted`);
+
+            return project;
+          } 
+          catch (error) {
+            // In case of an error, return the project as is without details
+            console.error('getting project details', error);
+            return project;
+          }
+        })
+      );
+    })
+  );
+}
+
+const updateDevs = async (developers) => {
+  try {
+    const res = await client.request("GET /repos/SaudiOpenSourceCommunity/SaudiOSS/contents/devs.json");
+    const { sha, encoding } = res.data;
+
+    client.request("PUT /repos/SaudiOpenSourceCommunity/SaudiOSS/contents/devs.json",{
+        message: "Update devs.json",
+        content: Buffer.from(JSON.stringify(developers, null, 4), "utf-8").toString(encoding),
+        sha
+      }
+    );
+  } 
+  catch (error) {
+    console.error('updating devs.json', error);
+  }
+};
+
+const main = async () => {
+  // Update the readme
+  await updateReadme(newTemplate);
+
+  // adding projects details
+  const updatedDevs = await getProjectsDescriptions(devs);
+  
+  // Update the devs.json file with the new projects details
+  await updateDevs(updatedDevs);
+}
+
+main();
